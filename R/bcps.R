55ba2604e15cb002e409d7b7b9bc68d183702c1c
Eli Pousson <eli.pousson@gmail.com>
2021-02-13 21:51:43 -0500

    Rename to BCPSS

--- R/bcps.R
+++
@@ -1,9 +0,0 @@
-#' BCPS: A package with data on Baltimore City Public Schools.
-#'
-#' The bcps package includes demographic, enrollment, and survey data related to
-#' Baltimore City Public Schools. Additional data from the Maryland State Department
-#' of Education and other sources will be added soon.
-#'
-#' @docType package
-#' @name bcps
-NULL
---
+++ R/bcpss.R
@@ -0,0 +1,9 @@
+#' BCPSS: A package with data on Baltimore City Public School System.
+#'
+#' The bcpss package includes demographic, enrollment, and survey data related to the
+#' Baltimore City Public School System (BCPSS). Additional data from the Maryland State Department
+#' of Education and other sources will be added soon.
+#'
+#' @docType package
+#' @name bcpss
+NULL
